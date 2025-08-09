3b71f1f03ec6af1c8d4ddb77d2a5f16acbb54a93
Hariharan.s <hreehs1428@gmail.com>
2025-08-10 03:54:23 +0530

    after Struggle Log in authentication & database Works

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/assets/logo.png'
+import { login,signup } from '../../firebase'
 
 
 const Login = () => {
 
 const [signState,setSignState] =useState('Sign In')
+const [name,setName] = useState('')
+const [password,setPassword] = useState('')
+const [email,setEmail] = useState('')
 
+const user_auth =async (event)=>{
+  if(signState==='Sign In'){
+    await login(email,password)
+  }else{
+    await(signup(name,email,password))
+  }
+}
 
 
   return (
@@ -15,11 +26,11 @@ const [signState,setSignState] =useState('Sign In')
       <div className="login-form">
         <h1>{signState}</h1>
         <form action="">
-          {signState==='Sign Up'? <input type='text' placeholder='Your Name'/>:<></>}
+          {signState==='Sign Up'? <input value={name} onChange={(e)=>{setName(e.target.value)}} type='text' placeholder='Your Name'/>:<></>}
        
-         <input type='email' placeholder='Email'/>
-          <input type='password'  placeholder='Password'/>
-          <button>Sign up</button>
+         <input value={email} onChange={(e)=>{setEmail(e.target.value)}} type='email' placeholder='Email'/>
+          <input value={password} onChange={(e)=>{setPassword(e.target.value)}} type='password'  placeholder='Password'/>
+          <button onClick={user_auth} type='submit'>Sign up</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
